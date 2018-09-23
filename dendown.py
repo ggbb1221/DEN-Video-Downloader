e870385a4f75315ff0aee534ed2779144b6cd0b5
ggbb1221 <42948353+ggbb1221@users.noreply.github.com>
2018-09-23 16:03:26 -0700

    Minor fixes
    Add support for downloading longer lectures (e.g. 3-hour lectures).
    Fix URL resolving for new AWS URLs.

--- dendown.py
+++ dendown.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     '''Initialize arguments.'''
     init_url = sys.argv[1]
     init_url_prefix = init_url.split('/playlist')[0]
-    file_name_parts = init_url.split('/')[6].split('_')
+    file_name_parts = init_url.split('/')[7].split('_')
     file_name = file_name_parts[0] + '_' + file_name_parts[1][-8:]
 
     '''Resolve M3U8 playlist attributes.'''
@@ -30,7 +30,7 @@ if __name__ == '__main__':
     '''Download video segments and concatenate together into a whole video.'''
     timestamp = int(time.time() * 100)
     os.system('mkdir ./temp_%s/' % timestamp)
-    os.system('curl -o ./temp_%s/part_#1.ts %s/%s\[000-%d\].ts'
+    os.system('curl -o ./temp_%s/part_#1.ts %s/%s\[0000-%d\].ts'
               % (timestamp, init_url_prefix, video_segment_prefix, num_total_segments - 1))
     os.system('cat ./temp_%s/part* > %s.ts' % (timestamp, file_name))
     os.system('rm -rf ./temp_%s/' % timestamp)
